5f909dacd7cbc017f2c45ceaa1340c9c2500b134
aj-ames <akash.james2096@gmail.com>
2017-04-11 21:12:44 +0530

    TrackMe: Add Receiving Location and Marking on Map

---
+++ app/src/main/java/com/aj_ames/trackme/AppCont.java
@@ -0,0 +1,51 @@
+package com.aj_ames.trackme;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class AppCont extends Application {
+
+    public static final String TAG = AppCont.class.getSimpleName();
+
+    private RequestQueue mRequestQueue;
+
+    private static AppCont mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized AppCont getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
--- app/src/main/java/com/aj_ames/trackme/MapsActivity.java
+++ app/src/main/java/com/aj_ames/trackme/MapsActivity.java
@@ -1,18 +1,49 @@
 package com.aj_ames.trackme;
 
+import android.app.ProgressDialog;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.List;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private TrackGPS gps;
+    double longitude, newLat, newLng, lat_url,lng_url;
+    double latitude;
+    private static String TAG = MapsActivity.class.getSimpleName();
+    FloatingActionButton locator;
+    public String url="http://api.thingspeak.com/channels/255333/feed/last.json";
+    private ProgressDialog pDialog;
+    public LatLng newlatlng,latLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +53,151 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        gps = new TrackGPS(MapsActivity.this);
+
+
+        if(gps.canGetLocation()){
+
+
+            longitude = gps.getLongitude();
+            latitude = gps .getLatitude();
+
+            Toast.makeText(getApplicationContext(),"Longitude:"+Double.toString(longitude)+"\nLatitude:"+Double.toString(latitude),Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            gps.showSettingsAlert();
+        }
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        /*SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    */
+
+        newlatlng = new LatLng(lat_url, lng_url);
+        locator = (FloatingActionButton) findViewById(R.id.fab);
+        locator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addOldMark(lat_url, lng_url);
+            }
+        });
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Please wait...");
+        pDialog.setCancelable(false);
+        makeJsonObjectRequest();
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        Geocoder gc = new Geocoder(MapsActivity.this);
+        List<Address> list = null;
+        try {
+            list = gc.getFromLocation(latitude,longitude,1);
+            newLat = latitude;
+            newLng = longitude;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        android.location.Address add =   list.get(0);
+        String addressLine1 = add.getAddressLine(1);
+        String addressLine2 = add.getAddressLine(2);
+        latLng= new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(latLng).title(addressLine1).snippet(addressLine2)).setVisible(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 22));
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Zoom in, animating the camera.
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(16), 2000, null);
+    }
+
+    private void addOldMark(double lat, double lng) {
+        LatLng ll = new LatLng(lat, lng);
+
+        Geocoder gc = new Geocoder(MapsActivity.this);
+
+        List<android.location.Address> list = null;
+
+        //LatLng latLng = marker.getPosition();
+
+        try {
+            list = gc.getFromLocation(13.119053, 77.578741, 1);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        android.location.Address add = list.get(0);
+        String addressLine1 = add.getAddressLine(0);
+        String addressLine2 = add.getAddressLine(2);
+        mMap.addMarker(new MarkerOptions().position(ll).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)).title("Child's location ").snippet(addressLine1));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newlatlng, 22));
+
+        // Zoom in, animating the camera.
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(16), 2000, null);
+
+
+        float[] results = new float[1];
+        Location.distanceBetween(latitude, longitude, 13.119053, 77.578741, results);
+        float distance = results[0];
+
+        double dist = (double) distance / 1000;
+        DecimalFormat f = new DecimalFormat("##.00");
+
+        Toast.makeText(getApplicationContext(), "Distance to Travel is: " + f.format(dist) + " km", Toast.LENGTH_LONG).show();
+
+    }
+
+    private void makeJsonObjectRequest() {
+
+        showpDialog();
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+
+                try {
+
+                    lat_url = response.getDouble("field1");
+                    lng_url = response.getDouble("field2");
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+                hidepDialog();
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+                // hide the progress dialog
+                hidepDialog();
+            }
+        });
+
+        // Adding request to request queue
+        AppCont.getInstance().addToRequestQueue(jsonObjReq);
+    }
+
+    private void showpDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hidepDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
     }
 }
